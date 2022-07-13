b3e1b7967d586b9ab17c65ec5f1385534261ba5c
zarak khan <zarakkhan421@gmail.com>
2022-07-13 12:36:19 +0500

    scripts added

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-
+require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server running on ${PORT}`));
